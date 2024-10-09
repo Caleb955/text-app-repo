7cb5bc69a9c7402f08ccaf1fac58a899a3a26132
Caleb955 <161380361+Caleb955@users.noreply.github.com>
2024-10-09 11:09:16 +0100

    when a user save a text it save it in local storage, adds it to an array and loop it so we can see the data in the array.

--- script.js
+++ script.js
@@ -1,27 +1,80 @@
 const textareaElement = document.getElementById('data-input');
 const bodyElement = document.documentElement;
+const textContainer = document.querySelector('.js-data-list');
 
-const textArray = [{
-    textContent: undefined,
-    id:undefined
-}];
+let textArray = JSON.parse(localStorage.getItem('arraylist'));
 
+let counterId = JSON.parse(localStorage.getItem('counter'));
 
-textareaElement.value = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta numquam reprehenderit eligendi nisi et asperiores perspiciatis rem ratione minima sint, id repellat quia quo sequi, provident eum, nulla dolores quos.Sunt nostrum exercitationem neque amet voluptatum possimus atque quasi dolorem, quos laudantium ea deserunt dolorum necessitatibus illum temporibus magni repellendus architecto facere magnam laboriosam, minus quidem! Earum vero facilis fuga.
-Nihil eius quo ad commodi! Animi doloribus dolores eius, voluptatum quod excepturi vel rerum aliquid reprehenderit esse pariatur earum! Quidem consectetur earum impedit magnam quod ea saepe animi similique ducimus?
-Quia exercitationem sunt cumque sapiente minus tenetur. Consequuntur, quo dolores quam doloremque beatae aut dolorum quas laborum expedita alias fugiat animi repudiandae ipsa eaque sequi harum molestias neque autem architecto!
-Dolorum, esse sed. Suscipit minima, ducimus tempora dignissimos neque magni? Tempore eaque id doloremque. Nam possimus nulla quos ullam aliquid! Modi eveniet impedit, facere perspiciatis quo voluptatibus consequuntur at quis?`;
+if (!textArray) {
+    textArray = [{
+        content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque facere expedita, sint in tenetur qui soluta iusto mollitia dolorum ipsum doloribus? Illum possimus eligendi est sunt, provident culpa. Quidem, placeat?
+        Suscipit repellendus rerum necessitatibus voluptas! Deleniti doloremque minima voluptatum sunt, totam porro necessitatibus, dolore exercitationem deserunt repudiandae excepturi. Quia in cumque, praesentium impedit laudantium iusto porro dicta saepe placeat autem.`,
+        id: 1
+    }]
+}
 
 window.addEventListener('click', (event) => {
     if (event.target.matches('textarea')) {
         return;
     } else if (event.target.matches('.js-plus-sign')) {
-        console.log('ok')
         bodyElement.toggleAttribute('plus');
     } else if (event.target.matches('#save-btn')) {
-        console.log(textareaElement.value);
-        bodyElement.toggleAttribute('plus');
+        passData();
     } else if (event.target.matches('.outer-shell')) {
         bodyElement.toggleAttribute('plus')
     }
 });
+
+function passData() {
+    const data = textareaElement.value;
+
+    if (!data) {
+        return;
+    } else {
+        textArray.unshift({
+            content: data,
+            id: counterId += 1
+        });
+    
+        displayData();
+    
+        saveToStorage('counter', counterId);
+    
+        textareaElement.value = '';
+
+        bodyElement.toggleAttribute('plus');
+    }
+}
+
+function saveToStorage(name, variable) {
+    localStorage.setItem(name, JSON.stringify(variable));
+}
+
+function displayData() {
+    let textHTML = '';
+
+    textArray.forEach((textcontent) => {
+        textHTML += `
+            <div id="list-item-container" class="list-item-container" data-id=${textcontent.id}>
+                <div class="list-item">
+                ${textcontent.content}
+                </div>
+            </div>
+        `;
+    });
+
+    textContainer.innerHTML = textHTML;
+
+    saveToStorage('arraylist', textArray);
+
+    document.querySelectorAll('.list-item-container')
+        .forEach((container) => {
+            container.addEventListener('click', () => {
+                const {id} = container.dataset;
+                console.log(id);
+            })
+        });
+}
+
+displayData();
